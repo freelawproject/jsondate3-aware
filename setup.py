931aaee0572aef8193c397787c7ddabe5c0f7634
Michael Lissner <mlissner@michaeljaylissner.com>
2020-05-15 17:06:14 -0700

    feat(formalize): Add setup.cfg for doing releases

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ import setuptools
 
 
 setuptools.setup(
-    name="jsondate3",
-    version="0.1.4",
-    url="https://github.com/nitz14/jsondate",
-    license="MIT",
-    author="Rick Harris, Maciej nitZ Krol",
-    author_email="rconradharris@gmail.com, nitz@o2.pl",
+    name="jsondate3-aware",
+    version="0.1.0",
+    url="https://github.com/freelawproject/jsondate3-aware",
+    license="BSD",
+    author="Rick Harris, Maciej nitZ Krol, Free Law Project",
+    author_email="rconradharris@gmail.com, nitz@o2.pl, info@free.law",
     description="JSON with datetime support",
     long_description=__doc__,
     packages=setuptools.find_packages(),
